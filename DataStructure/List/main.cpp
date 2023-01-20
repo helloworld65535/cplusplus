976425ad48675b25ca14d48ca14310f5dbeaba9a
qiu1248 <2453424080@qq.com>
2023-01-20 11:52:18 +0800

    test

--- DataStructure/List/main.cpp
+++ DataStructure/List/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "ArrayList.hpp"
+#include "LinkedList.hpp"
 
 using namespace std;
 
@@ -10,14 +11,18 @@ void Print(const int &e)
 
 int main()
 {
-
-
-    ArrayList<int> arrayList;
+    LinkedList<int> arrayList;
     for (int i = 0; i < 10; i++)
     {
         arrayList.Insert(arrayList.Length() + 1, i);
     }
     arrayList.Traverse(Print);
-    std::cout << std::endl;
+    cout << endl;
+    for (int i = 0; i < 4; ++i)
+    {
+        arrayList.Delete(arrayList.Length());
+    }
+    arrayList.Traverse(Print);
+    cout << endl;
     return 0;
 }
