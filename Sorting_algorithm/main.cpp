bcedbfd033855c601e641ad0d61ed8797b1f5c5d
qiu1248 <2453424080@qq.com>
2023-01-22 22:28:58 +0800

    完成

--- Sorting_algorithm/main.cpp
+++ Sorting_algorithm/main.cpp
@@ -4,18 +4,19 @@
 #include <ctime>
 // #include <cstdlib>
 #include "InsertSort.h"
-#define log 1
 
-int main(void)
+#define TEST_NUMBER 10
+
+bool CheckSort(void (*SortFunc)(std::vector<int> &), bool Log)
 {
 
     std::vector<int> array;
-    srand(time(0));
+    srand((unsigned int)time(0));
     for (int i = 0; i < 20; i++)
     {
         array.push_back((rand() % 201) - 100); // 随机添加[-100,100]范围内的数
     }
-    if (log)
+    if (Log)
     {
         std::cout << "排序前：";
         std::for_each(array.begin(), array.end(), [](int i) -> void
@@ -29,27 +30,37 @@ int main(void)
     /* 排序操作 */
     // InsertSort i_sort(InsertSort::Binary);
     // i_sort(array);
-    InsertSort(InsertSort::Shell)(array);
+    SortFunc(array);
 
-    if (log)
+    if (Log)
     {
         std::cout << "排序后：";
         std::for_each(array.begin(), array.end(), [](int i) -> void
                       { printf("%3d ", i); });
         std::cout << std::endl;
-    }
 
-    if (log)
-    {
         std::cout << "  答案：";
         std::for_each(check_array.begin(), check_array.end(), [](int i) -> void
                       { printf("%3d ", i); });
         std::cout << std::endl;
+        if (check_array == array)
+        {
+            std::cout << "  正确" << std::endl;
+        }
     }
-    if (check_array == array)
+
+    return (array == check_array);
+}
+
+int main(void)
+{
+    int TestResult = 0;
+    for (int i = 0; i < TEST_NUMBER; i++)
     {
-        std::cout << "  正确" << std::endl;
+        if (CheckSort(InsertSort::ShellInsertSort, true))
+            ++TestResult;
     }
-    // return (array == check_array);
+    std::cout << "测试结果：" << TestResult << "/" << TEST_NUMBER << std::endl;
+
     return 0;
 }
