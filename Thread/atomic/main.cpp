8f6ecf4355c26fdc8bc4b1c3b3228b16d73d6fe7
qiu1248 <2453424080@qq.com>
2023-03-03 17:35:49 +0800

    atomic

--- Thread/atomic/main.cpp
+++ Thread/atomic/main.cpp
@@ -1,46 +1,33 @@
 #include <iostream>
 #include <thread>
 #include <atomic>
-#include <functional>
-using namespace std;
 
-struct Counter
+void increment(std::atomic<int> &number)
 {
-    void increment()
+    for (int i = 0; i < 500; i++)
     {
-        for (int i = 0; i < 10; ++i)
-        {
-            m_value++;
-            cout << "increment number: " << m_value
-                << ", theadID: " << this_thread::get_id() << endl;
-            this_thread::sleep_for(chrono::milliseconds(100));
-        }
+        int num = ++number;
+        printf("[increment]:%d\n", num);
     }
+}
 
-    void decrement()
-    {
-        for (int i = 0; i < 10; ++i)
-        {
-            m_value--;
-            cout << "decrement number: " << m_value
-                << ", theadID: " << this_thread::get_id() << endl;
-            this_thread::sleep_for(chrono::milliseconds(100));
-        }
-    }
-    // atomic<int> == atomic_int
-    atomic_int m_value = 0;
-};
-
-int main()
+void decrement(std::atomic<int> &number)
 {
-    Counter c;
-    auto increment = bind(&Counter::increment, &c);
-    auto decrement = bind(&Counter::decrement, &c);
-    thread t1(increment);
-    thread t2(decrement);
+    for (int i = 0; i < 500; i++)
+    {
+        int num = --number;
+        printf("[decrement]:%d\n", num);
+    }
+}
 
-    t1.join();
-    t2.join();
+int main(void)
+{
+    std::atomic<int> number = 0;
+    // std::ref 引用传递
+    std::thread thread_in(increment, std::ref(number));
+    std::thread thread_de(decrement, std::ref(number));
+    thread_in.join();
+    thread_de.join();
 
     return 0;
 }
